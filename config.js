3aa029e5023eae29a3f95e8794bab8ecd56fdc8b
rasindus <160561869+rasindus@users.noreply.github.com>
2025-06-11 17:38:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "1rViBQaT#6rgH5yn3RJuJWZJtcQIcid-pmytYWra77gXkay71jiU":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "ENTER YOUR MONGO DB URL",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
